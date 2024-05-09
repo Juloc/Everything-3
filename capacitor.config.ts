e17d1bcce0e31c40c2b21b9e33d7322aa746eccc
Juloc <juli.hiresch@gmail.com>
2024-05-09 10:08:55 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'everything.v3',
+  appName: 'everything-3',
+  webDir: 'www'
+};
+
+export default config;
